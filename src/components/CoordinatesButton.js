0930b745ce5342e08df3bf881e389c8cbcf77556
vincent <vincent.arteaga@gmail.com>
2021-06-09 22:46:59 -0700

    assignment complete

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,35 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+
+class CoordinatesButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    handleClick = (e) => {
+        let coord = [e.clientX, e.clientY];
+        this.props.onReceiveCoordinates(coord)
+        console.log('this is cord', coord)
+    }
+
+    render() { 
+        return ( <button onClick={this.handleClick} >button</button> );
+    }
+}
+ 
+export default CoordinatesButton;
+
+
+
+//  CoordinatesButton 
+// In the components/CoordinatesButton.js file, create a CoordinatesButton React component.
+
+// This component takes in one prop: onReceiveCoordinates. This prop is a function passed down from index.js. This function is currently just logging whatever is passed into it.
+
+// Within CoordinatesButton, render a button. On click of the button, create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
+
+// Pass this event data in as the argument for the onReceiveCoordinates prop.
+
+// If successful, the current x,y position of your mouse should be logged.
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,33 @@
 // Code DelayedButton Component Here
+
+import React, { Component } from 'react'
+
+
+
+class DelayedButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    handleClick = (e) => {
+        e.persist()
+        window.setTimeout(() => {
+            this.props.onDelayedClick(e)
+        }, this.props.delay);
+    }  
+    render() { 
+        return ( <button onClick={this.handleClick}>DELAY</button> );
+    }
+}
+ 
+export default DelayedButton;
+// DelayedButton
+// In the components/DelayedButton.js file, create a DelayedButton React component
+
+// This component takes two props: onDelayedClick (a function), and delay (a number).
+
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+
+// If successful, the event will be logged to the console once the timeout has finished.
+
+// Hint: If you having trouble with this feature, remember event pooling in React. By the time the setTimeout fires, the event object will have already been returned to the pool. So how can we fix that?
